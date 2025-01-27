885aa1e8f9d4e7c3cc9cbfc1f778b2b1b4a26f74
MatosRicardo <ricardoribeirodematos@gmail.com>
2025-01-27 19:49:34 -0300

    feat: adding data functionality

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-'use strict';
+"use strict";
+
+const score0EL = document.querySelector("#score--0");
+const score1EL = document.getElementById("score--1");
+const diceEL = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+diceEL.classList.add("hidden");
+
+btnRoll.addEventListener("click", function () {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  diceEL.classList.remove("hidden");
+  diceEL.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    // Add dice to current score
+  } else {
+    // Switch to next player
+  }
+});
