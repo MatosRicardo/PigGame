8418ee1fd555881641a39a9d292b8914f5151af6
MatosRicardo <ricardoribeirodematos@gmail.com>
2025-01-27 20:16:50 -0300

    feat: adding player changes

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 "use strict";
 
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0EL = document.querySelector("#score--0");
 const score1EL = document.getElementById("score--1");
+const current0EL = document.getElementById("current--0");
+const current1EL = document.getElementById("current--1");
+
 const diceEL = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 score0EL.textContent = 0;
 score1EL.textContent = 0;
@@ -19,8 +28,13 @@ btnRoll.addEventListener("click", function () {
   diceEL.src = `dice-${dice}.png`;
 
   if (dice !== 1) {
-    // Add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
   }
 });
